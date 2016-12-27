7e14b352fd38e7c1c2652d351ceb80f102f20982
Jeremy Meyer <jeremythedev@gmail.com>
2016-12-26 19:15:05 -0800

    feat: ex3

---
+++ ex3.sql
@@ -0,0 +1,9 @@
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, "Jeremy", "Meyer", 22);
+
+INSERT INTO pet (name, breed, age, is_dead)
+  VALUES ("Spot", "Beagle", 3, 0);
+
+INSERT INTO person_pet (person_id, pet_id)
+  VALUES (0, 0);
+
